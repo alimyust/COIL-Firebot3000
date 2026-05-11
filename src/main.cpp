1a8c5c5b0582e3950202668981b1b575cf8ccd16
alimyust <alimyust@gmail.com>
2026-05-11 12:19:23 -0400

    Fixed syntax bug to make project buildable with tagtronics

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
-
+#include "DualHWPwm.hpp"
+  
+DualHardwarePWM pwm(9, 5);  // Pin 9 for drive, Pin 5 for steering
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pwm.begin(60);  // 60 Hz frequency
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  pwm.setDutyCycle1(75);   // Drive at 75%
+  pwm.setDutyCycle2(50);   // Steer at 50%
+  delay(1000);
 }
