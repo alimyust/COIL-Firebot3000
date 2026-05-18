1e0f6163687c5c771b45428045c10581c66c10af
alimyust <alimyust@gmail.com>
2026-05-18 15:52:01 +0200

    added controller code

--- src/controller/Controller.hpp
+++ src/controller/Controller.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-#include "../../lib/tagtronics/RFComm.hpp"
-#include "../robot/ProtocolLayer.hpp"
-
-// Controller class for reading joystick and sending commands via ProtocolLayer
+#include "RFComm.hpp"
 
 class Controller {
 public:
@@ -14,14 +11,26 @@ public:
     void update();
 
 private:
+    enum CommandId : uint8_t {
+        THROTTLE = 0x05,
+        STEERING_DUTY = 0x06,
+    };
+
+    static constexpr uint8_t ROBOT_NODE_ID = 1;
+    static constexpr uint8_t CONTROLLER_NODE_ID = 2;
+    static constexpr float RF_FREQUENCY_MHZ = 868.0f;
+    static constexpr uint16_t ANALOG_CENTER = 512;
+    static constexpr uint16_t JOYSTICK_DEADZONE = 32;
+    static constexpr uint32_t SEND_INTERVAL_MS = 50;
+
     RF69_Comm _comm;
-    ProtocolLayer _protocol;
     uint8_t _joyXPin;
     uint8_t _joyYPin;
+    unsigned long _lastSendTime;
+    uint8_t _lastThrottleDuty;
+    uint8_t _lastSteeringDuty;
+    bool _hasSentState;
 
-    const uint16_t JOYSTICK_DEADZONE = 32;
-    const uint32_t SEND_INTERVAL_MS = 50;
-    unsigned long _lastSendTime = 0;
-
-    uint8_t mapJoystickToDuty(uint8_t analogPin);
+    uint8_t mapAnalogToDuty(uint8_t analogPin, bool invert = false) const;
+    bool sendDuty(uint8_t command, uint8_t duty);
 };
