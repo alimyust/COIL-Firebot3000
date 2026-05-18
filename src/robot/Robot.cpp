a59d586284605bd072cabfae09738d0e95024a49
alimyust <alimyust@gmail.com>
2026-05-18 15:39:54 +0200

    changes that don't work

--- src/robot/Robot.cpp
+++ src/robot/Robot.cpp
@@ -1,33 +1,70 @@
 #include <Arduino.h>
 #include "DualHWPwm.hpp"
-#include "ProtocolLayer.hpp"
-#include "CommandHandler.hpp"
+#include "RFComm.hpp"
 
-const uint8_t DRIVE_PWM_PIN = 9;
-const uint8_t STEER_PWM_PIN = 5;
+namespace {
+
+constexpr uint8_t DRIVE_PWM_PIN = 9;
+constexpr uint8_t STEER_PWM_PIN = 5;
+constexpr uint8_t ROBOT_NODE_ID = 1;
+constexpr float RF_FREQUENCY_MHZ = 868.0f;
+constexpr uint32_t PWM_FREQUENCY_HZ = 50;
+
+enum CommandId : uint8_t {
+    MOTOR_SPEED = 0x01,
+    STEERING = 0x02,
+    BATTERY = 0x03,
+    HEARTBEAT = 0x04,
+    THROTTLE = 0x05,
+    STEERING_DUTY = 0x06,
+};
 
 DualHardwarePWM pwm(DRIVE_PWM_PIN, STEER_PWM_PIN);
-RF69_Comm comm(1, 868.0f);
-ProtocolLayer protocol(comm);
-CommandHandler commandHandler(pwm);
+RF69_Comm comm(ROBOT_NODE_ID, RF_FREQUENCY_MHZ);
 
-void throttleCallback(uint8_t duty) {
-    commandHandler.setThrottleDuty(duty);
+void setThrottleDuty(uint8_t duty) {
+    pwm.setDutyCycle1(duty);
+    Serial.print("Throttle duty set to: ");
+    Serial.println(duty);
 }
 
-void steeringCallback(uint8_t duty) {
-    commandHandler.setSteeringDuty(duty);
+void setSteeringDuty(uint8_t duty) {
+    pwm.setDutyCycle2(duty);
+    Serial.print("Steering duty set to: ");
+    Serial.println(duty);
+}
+
+void handlePacket(RF69_Packet &packet) {
+    switch (packet.command) {
+        case THROTTLE:
+            setThrottleDuty(static_cast<uint8_t>(atoi(packet.payload)));
+            break;
+        case STEERING_DUTY:
+            setSteeringDuty(static_cast<uint8_t>(atoi(packet.payload)));
+            break;
+        case MOTOR_SPEED:
+        case STEERING:
+        case BATTERY:
+        case HEARTBEAT:
+        default:
+            break;
+    }
+}
+
+void receiveCallback(RF69_Packet &packet) {
+    handlePacket(packet);
+}
+
 }
 
 void setup() {
     Serial.begin(9600);
+    comm.set_receive_handler(receiveCallback);
     comm.begin(nullptr, "encryptionkey16");
-    commandHandler.initialize(50); // 50 Hz for servos
-    protocol.setThrottleCallback(throttleCallback);
-    protocol.setSteeringDutyCallback(steeringCallback);
+    pwm.begin(PWM_FREQUENCY_HZ);
     Serial.println("Robot started");
 }
 
 void loop() {
-    protocol.process();
+    comm.update();
 }
